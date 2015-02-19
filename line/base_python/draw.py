d213846398e603a10a02777fc39fb79c33d8fdf0
Enigmamemory <ylsdhu@hotmail.com>
2015-02-18 21:42:08 -0800

    Code complete - for now?

--- line/base_python/draw.py
+++ line/base_python/draw.py
@@ -31,10 +31,18 @@ def draw_line( screen, x0, y0, x1, y1, color ):
     y = y0
     A = 2*(y1-y0)
     B = -2*(x1-x0)
-    m = A / B * -1
+    if (B != 0):
+        m = A / B * -1
     d = A + (B/2)
-    if (B == 0):
-        return "lol"
+    if (x > x1):
+        draw_line(screen, x1, y1, x0, y0, color)
+    elif (B == 0):
+        if (y >= y1):
+            y = y1
+            y1 = y0 #switches order of points so it can still be drawn bottom up
+        while (y <= y1):
+            plot(screen,color,x,y)
+            y += 1
         #Need a code that draws vertical lines
     elif (m > 1):
         while (y <= y1):
@@ -57,34 +65,44 @@ def draw_line( screen, x0, y0, x1, y1, color ):
             plot(screen,color,x,y)
             if d < 0:
                 y -= 1
-                d += B
+                d -= B
             x += 1
             d += A
-            check = [x,y]
-            print check
+            #check = [x,y]
+            #print check
     elif (m <= -1):
-        print "Got to this point :^)"
+        #print "Got to this point :^)"
         while (y >= y1):
             plot(screen,color,x,y)
             if d > 0:
                 x += 1
                 d += A
             y -= 1
-            d += B
+            d -= B
             #check = [x,y]
             #print check
 
 
         
-
+'''
 test = new_matrix(0,0)
 add_edge(test,0,0,0,50,10,160)
 add_edge(test,0,0,0,50,50,0)
 add_edge(test,0,0,0,20,100,0)
 add_edge(test,0,100,50,50,0,0)
 add_edge(test,0,50,0,100,0,0)
+add_edge(test,50,0,0,50,100,0)
+add_edge(test,25,100,0,25,0,0)
+add_edge(test,200,160,0,150,150,0)
+add_edge(test,170,250,0,150,150,0)
+add_edge(test,200,200,0,150,150,0)
+add_edge(test,200,150,0,150,250,0)
+add_edge(test,200,250,0,200,150,0)
+add_edge(test,250,150,0,150,200,0)
+
 #print_matrix(test)
 blah = new_screen()
 nyeh = [100,100,0]
 draw_lines(test,blah,nyeh)
 display(blah)
+'''
